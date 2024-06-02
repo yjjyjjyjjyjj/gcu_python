f85229e8492d309140e4ded84e49c60c044d7cf4
JeonYoungJun <yj951442@gmail.com>
2024-06-02 16:08:13 +0900

    과제 4 소스코드보고서 작성 완료.

--- ch12/p565_shape2.py
+++ ch12/p565_shape2.py
@@ -5,13 +5,15 @@ class Shape:
     def getArea(self):
         raise NotImplementedError("이것은 추상메소드입니다.")
 
+
 class Circle(Shape):
     def __init__(self, name, radius):
         super().__init__(name)
         self.radius = radius
 
     def getArea(self):
-        return 3.141592*self.radius**2
+        return 3.141592 * self.radius ** 2
+
 
 class Rectangle(Shape):
     def __init__(self, name, width, height):
@@ -20,8 +22,9 @@ class Rectangle(Shape):
         self.height = height
 
     def getArea(self):
-        return self.width*self.height
+        return self.width * self.height
 
-shapeList = [Circle("c1",10), Rectangle("r1", 10, 10)]
+
+shapeList = [Circle("c1", 10), Rectangle("r1", 10, 10)]
 for s in shapeList:
     print(s.getArea())
